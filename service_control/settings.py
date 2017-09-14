a4c55600d074f3c4c9fc09e2b1524cd13fc854f4
Fabio Teixeira <fabio1079@gmail.com>
2017-09-14 13:29:03 -0300

    Enable authtoken
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Hugo Catarino <hugocatarino1@gmail.com>

--- service_control/settings.py
+++ service_control/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party
+    'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken',
+
     # My apps
     'core'
 ]
@@ -45,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # corsheaders
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_WHITELIST = []
