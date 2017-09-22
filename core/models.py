88ed16866af3bfc40d1fb84a5cc27562f064af5b
Fabio Teixeira <fabio1079@gmail.com>
2017-09-22 10:33:25 -0300

    change related_name of profile from person to profile

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, related_name='person', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
 
     def __str__(self):
         return "Profile of : %s" % self.user.username
