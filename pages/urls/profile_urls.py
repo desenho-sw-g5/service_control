f474a238fdcfbc9deabf6c027de55295eb851981
Fabio Teixeira <fabio1079@gmail.com>
2017-09-25 16:58:56 -0300

    Add user registration, login and logout

--- pages/urls/profile_urls.py
+++ pages/urls/profile_urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.profile_show, name='pages_profile_show'),
     url(r'^create/$', views.profile_create, name='pages_profile_create'),
     url(r'^login/$', views.profile_login, name='pages_profile_login'),
+    url(r'^logout/$', views.profile_logout, name='pages_profile_logout'),
     url(r'^signup/$', views.profile_signup, name='pages_profile_signup'),
+    url(r'^register/$', views.profile_register, name='pages_profile_register'),
 ]
--- pages/views/profile.py
+++ pages/views/profile.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate, login, logout
+
+from rest_framework.authtoken.models import Token
 
 from core.models import Profile, User
+
 
 def profile_list(request: HttpRequest) -> HttpResponse:
     profiles = Profile.objects.select_related('user').all()
@@ -12,6 +17,7 @@ def profile_list(request: HttpRequest) -> HttpResponse:
     }
 
     return render(request, "profile/list.html", context)
+
 
 def profile_show(request: HttpRequest, pk: int) -> HttpResponse:
     profile = get_object_or_404(Profile.objects.select_related('user'), pk=pk)
@@ -26,8 +32,64 @@ def profile_show(request: HttpRequest, pk: int) -> HttpResponse:
 def profile_create(request: HttpRequest) -> HttpResponse:
     return render(request, "profile/create.html", {})
 
+
 def profile_login(request: HttpRequest) -> HttpResponse:
+    if not request.user.is_anonymous():
+        return redirect("pages_profile_list")
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("pages_profile_list")
+
     return render(request, "profile/login.html", {})
+
+
+def profile_logout(request: HttpRequest) -> HttpResponse:
+    logout(request)
+
+    return redirect("pages_profile_login")
+
 
 def profile_signup(request: HttpRequest) -> HttpResponse:
     return render(request, "profile/signup.html", {})
+
+
+def profile_register(request: HttpRequest) -> HttpResponse:
+    if request.method != "POST":
+        return redirect("pages_profile_login")
+
+    user_data = {
+        'username': request.POST['username'],
+        'password': request.POST['password'],
+    }
+
+    try:
+        user = User(**user_data)
+        user.full_clean()
+        user.save()
+
+        Token.objects.create(user=user)
+
+        profile = Profile(user=user)
+        profile.full_clean()
+        profile.save()
+
+        login(request, user)
+
+        return redirect("pages_profile_list")
+    except ValidationError as validation_errors:
+        # This errors is a workaround becouse django and jinja2 does not allow
+        # a simple thing like "for err in errors['username']" in its template system...
+        # shame on you django, shame on you.
+        errors = {}
+
+        for key, value in dict(validation_errors).items():
+            errors['{}_errors'.format(key)] = value
+
+        return render(request, "profile/signup.html", errors)
