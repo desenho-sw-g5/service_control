c0ea5fbf360fe88b54929376daa3b531219a047e
Fabio Teixeira <fabio1079@gmail.com>
2017-09-26 14:23:37 -0300

    Add user edit page

--- pages/urls/profile_urls.py
+++ pages/urls/profile_urls.py
@@ -3,11 +3,27 @@ from django.conf.urls import url
 from pages import views
 
 urlpatterns = [
+    # Display all profile
     url(r'^$', views.profile_list, name='pages_profile_list'),
-    url(r'^show/(?P<profile_id>[0-9]+)/$', views.profile_show, name='pages_profile_show'),
+
+    # Display a single profile, given its user id
+    url(r'^show/(?P<user_id>[0-9]+)/$', views.profile_show, name='pages_profile_show'),
+
+    # Display the edit profile and user data page and send its data to profile_update page
     url(r'^edit/(?P<user_id>[0-9]+)/$', views.profile_edit, name='pages_profile_edit'),
+
+    # Do not display a page, just get the data from profile_edit, updates and redirect to edit page
+    url(r'^update/(?P<user_id>[0-9]+)/$', views.profile_update, name='pages_profile_update'),
+
+    # Display a loggin page
     url(r'^login/$', views.profile_login, name='pages_profile_login'),
+
+    # Do not display a page, just remove the current user from the session and redirect to loggin page
     url(r'^logout/$', views.profile_logout, name='pages_profile_logout'),
+
+    # Display a register form for a new user
     url(r'^signup/$', views.profile_signup, name='pages_profile_signup'),
+
+    # Do not display a page, just register the new user and redirect it to the loggin page
     url(r'^register/$', views.profile_register, name='pages_profile_register'),
 ]
--- pages/views/profile.py
+++ pages/views/profile.py
@@ -19,8 +19,8 @@ def profile_list(request: HttpRequest) -> HttpResponse:
     return render(request, "profile/list.html", context)
 
 
-def profile_show(request: HttpRequest, profile_id: int) -> HttpResponse:
-    profile = get_object_or_404(Profile.objects.select_related('user'), pk=profile_id)
+def profile_show(request: HttpRequest, user_id: int) -> HttpResponse:
+    profile = get_object_or_404(User.objects.select_related('profile'), pk=user_id).profile
 
     context = {
         'profile' : profile
@@ -30,7 +30,62 @@ def profile_show(request: HttpRequest, profile_id: int) -> HttpResponse:
 
 
 def profile_edit(request: HttpRequest, user_id: int) -> HttpResponse:
-    return render(request, "profile/edit.html", {})
+    # TODO: Only and Admin or Manager can edit other people's
+    # TODO: Only Admin and Manager can edit all members
+    # TODO: Only Admin can edit all managers
+
+    user = get_object_or_404(User.objects.select_related('profile'), pk=user_id)
+
+    return render(request, "profile/edit.html", {
+        'user': user,
+        'profile': user.profile
+    })
+
+
+def profile_update(request: HttpRequest, user_id: int) -> HttpResponse:
+    # TODO: The same as the profile_edit todos
+
+    if request.method == "POST":
+        first_name = request.POST['first_name'].strip()
+        last_name = request.POST['last_name'].strip()
+        email = request.POST['email'].strip()
+        password = request.POST['password'].strip()
+        password_changed = False # Django dont like when a user change its password
+
+        user = get_object_or_404(User.objects.select_related('profile'), pk=user_id)
+
+        # TODO: The must be a better way of doing this...
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+
+        if password != "":
+            user.set_password(password)
+            password_changed = True
+
+        try:
+            user.full_clean()
+            user.save()
+
+            if password_changed:
+                # For some reason, Django goes crazy when the user changes password
+                # and dont re-log in the system
+                logout(request)
+                login(request, user)
+
+        except ValidationError as validation_errors:
+            context = {
+                'user': user,
+                'profile': user.profile
+            }
+
+            for key, value in dict(validation_errors).items():
+                context['{}_errors'.format(key)] = value
+
+            return render(request, "profile/edit.html", context)
+
+
+    return redirect("pages_profile_edit", user_id=user.id)
 
 
 def profile_login(request: HttpRequest) -> HttpResponse:
